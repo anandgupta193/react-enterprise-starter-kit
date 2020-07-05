9fbbf6eb7b537ae7731863009abde5afd54595b5
Shubham Jain <jains1801@gmail.com>
2020-07-05 19:54:56 +0530

    axios-added

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,2 +1,9 @@
 export const INCREASE_COUNTER = 'Increase Counter';
 export const DECREASE_COUNTER = 'Decrease Counter';
+
+export const requestObject = {
+  url: '',
+  params: {},
+  data: {},
+  config: {},
+};
