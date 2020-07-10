460adb3525ff8c985bd7c5c0dfd8a551697a189e
unknown <shubhama664@gmail.com>
2020-07-11 03:16:04 +0530

    Story book

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Button } from '@storybook/react/demo';
 import { addCount, substractCount } from '../actions/CounterAction';
 import { INCREASE_COUNTER, DECREASE_COUNTER } from '../constants/AppConstants';
 
@@ -13,16 +14,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <button type="button"><Link to="/Home">Home</Link></button>
+      <Button type="button"><Link to="/Home">Home</Link></Button>
       <h1>
         { `Counter ${counter}` }
       </h1>
-      <button type="button" onClick={incrementCounter}>
+      <Button type="button" onClick={incrementCounter}>
         { INCREASE_COUNTER }
-      </button>
-      <button type="button" onClick={decrementCounter}>
+      </Button>
+      <Button type="button" onClick={decrementCounter}>
         { DECREASE_COUNTER }
-      </button>
+      </Button>
     </div>
   );
 };
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@storybook/react/demo';
 import { WELCOME_HEADING, BUTTON_TEXT } from '../../constants/HomeConstants';
 
 const Home = () => (
   <>
     <h1>{WELCOME_HEADING}</h1>
-    <button type="button"><Link to="/">{BUTTON_TEXT}</Link></button>
+    <Button type="button"><Link to="/">{BUTTON_TEXT}</Link></Button>
   </>
 );
 export default Home;
