74437e23a84006780ec682981b6ce42e6628a938
unknown <shubhama664@gmail.com>
2020-07-12 00:20:47 +0530

    Folder structure

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@storybook/react/demo';
+import Button from '../../pureComponents/Button/Button';
 import { WELCOME_HEADING, BUTTON_TEXT } from '../../constants/HomeConstants';
 
 const Home = () => (
   <>
     <h1>{WELCOME_HEADING}</h1>
-    <Button type="button"><Link to="/">{BUTTON_TEXT}</Link></Button>
+    <Button value={<Link to="/">{BUTTON_TEXT}</Link>} />
   </>
 );
 export default Home;
---
+++ src/components/home/Home.story.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../../pureComponents/Button/Button';
+
+export default { title: 'Home' };
+
+export const withText = () => <Button value="Hello" />;
+
+export const withEmoji = () => (
+  <Button value="hy" />
+);
