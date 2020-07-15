22ecd76435612b411b35547607ba987c98930f88
unknown <shubhama664@gmail.com>
2020-07-16 01:44:37 +0530

    Added extract-css plugin and purgecss

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../pureComponents/Button/Button';
 import { WELCOME_HEADING, BUTTON_TEXT } from '../../constants/HomeConstants';
+import homeStyle from './Home.scss';
 
 const Home = () => (
   <>
-    <h1>{WELCOME_HEADING}</h1>
+    <h1 className={homeStyle.size60}>
+      {WELCOME_HEADING}
+    </h1>
     <Button value={<Link to="/">{BUTTON_TEXT}</Link>} />
   </>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,49 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const CompressionPlugin = require('compression-webpack-plugin');
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const env = process.env.NODE_ENV;
 
 const commonPlugins = [
   new CopyPlugin({
-    patterns: [
-      { from: './public/index.html' },
-    ],
+    patterns: [{ from: './public/index.html' }],
+  }),
+  new ExtractCssChunks({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
   }),
 ];
 
 if (process.env.analyze) {
-  commonPlugins.push(new BundleAnalyzerPlugin({
-    openAnalyzer: true,
-    reportFilename: 'bundleReport.html',
-    analyzerMode: 'static',
-    token: 'c3e980d3ec23ddd626fecc110501e76a9a469461',
-  }));
+  commonPlugins.push(
+    new BundleAnalyzerPlugin({
+      openAnalyzer: true,
+      reportFilename: 'bundleReport.html',
+      analyzerMode: 'static',
+      token: 'c3e980d3ec23ddd626fecc110501e76a9a469461',
+    }),
+  );
 }
 
 if (process.env.NODE_ENV === 'production') {
-  commonPlugins.push(new CompressionPlugin({
-    algorithm: 'gzip',
-    filename: '[path].gz[query]',
-    test: /\.(js|jsx)$|\.css$|\.html$/,
-  }));
+  commonPlugins.push(
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      filename: '[path].gz[query]',
+      test: /\.(js|jsx)$|\.css$|\.html$/
+      ,
+    }),
+  );
+
+  commonPlugins.push(
+    new OptimizeCssAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+    }),
+  );
 }
 
 module.exports = {
@@ -40,6 +59,49 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: ExtractCssChunks.loader,
+            options: {
+              reloadAll: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: env === 'development',
+              modules: {
+                mode: 'local',
+                exportGlobals: true,
+                localIdentName: env === 'development' ? '[name]__[local]__[hash:base64:5]' : '[hash:base64:5]',
+                context: path.resolve(__dirname, 'src'),
+                hashPrefix: 'React Enterprice kit',
+              },
+            },
+          },
+          {
+            loader: '@americanexpress/purgecss-loader',
+            options: {
+              paths: [path.join(__dirname, 'src/**/*.{js,jsx}')],
+              whitelistPatternsChildren: [/:global$/],
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: env === 'development',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: env === 'development',
+            },
+          },
+        ],
       },
     ],
   },
