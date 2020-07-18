6a66cf8b494cb50484f8e85c5b0cdb102b2cc5c0
unknown <shubhama664@gmail.com>
2020-07-19 03:52:55 +0530

    Added Clean webpack plugin and designed home page

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,21 @@
+const path = require('path');
+
+// Export a function. Accept the base config as the only param.
 module.exports = {
   stories: ["../src/**/*.story.jsx"],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../src'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
