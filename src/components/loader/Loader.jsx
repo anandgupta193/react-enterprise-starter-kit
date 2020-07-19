1cf84023442add5959a26a908da94c4e38192b17
unknown <shubhama664@gmail.com>
2020-07-20 01:30:06 +0530

    Importing dynamic imports from separate files

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styles from './Loader.scss';
+import Styles from './Loader.scss';
 
 const Loader = () => (
-  <div className={styles.loader} />
+  <div className={Styles.loader} />
 );
 export default Loader;
