89a58e5133ea6d54a89355240d82a8f4cde11424
unknown <shubhama664@gmail.com>
2020-07-20 01:30:06 +0530

    Importing dynamic imports from separate files

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import homeStyle from './Home.scss';
-import { WELCOME_HEADING } from '../../constants/HomeConstants';
+import HomeStyle from './Home.scss';
+import { WELCOME_HEADING, IMG_ALT } from '../../constants/HomeConstants';
 // eslint-disable-next-line import/no-unresolved
 import ReactLogo from '../../assets/images/react.webp';
 
 const Home = () => (
   <>
-    <span className={homeStyle.forkongithub}>
+    <span className={HomeStyle.forkonGithub}>
       <a href="https://github.com/anandgupta193/react-enterprise-starter-kit">
         Fork on GitHub
       </a>
     </span>
-    <img className={homeStyle.logo} alt="React-Enterprise-Strater-Kit" src={ReactLogo} />
-    <h1 className={homeStyle.heading}>{WELCOME_HEADING}</h1>
+    <img className={HomeStyle.logo} alt={IMG_ALT} src={ReactLogo} />
+    <h1 className={HomeStyle.heading}>{WELCOME_HEADING}</h1>
   </>
 );
 export default Home;
