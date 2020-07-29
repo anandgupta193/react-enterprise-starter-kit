1a91d3935834edc972a4df9ffffaebff33b00ab6
unknown <shubhama664@gmail.com>
2020-07-29 12:46:23 +0530

    Added Styles to Welcome Page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Home.scss';
+import HomeStyles from './Home.scss';
 import {
   WELCOME_HEADING,
   IMG_ALT,
@@ -13,18 +13,18 @@ import ReactLogo from '../../assets/images/React.webp';
 import CloudsImage from '../../assets/images/wavesOpacity.svg';
 
 const Home = () => (
-  <>
-    <img className={styles.cloud} alt="cloud" src={CloudsImage} />
-    <span className={styles.forkonGithub}>
+  <div className={HomeStyles.app}>
+    <img alt="Clouds" src={CloudsImage} />
+    <span className={HomeStyles.forkonGithub}>
       <a href={REPO_URL}>{GITHUB_FORK_TEXT}</a>
     </span>
-    <img className={styles.logo} alt={IMG_ALT} src={ReactLogo} />
-    <h1 className={styles.heading}>{WELCOME_HEADING}</h1>
-    <div className={styles.badges}>
+    <img className={HomeStyles.logo} alt={IMG_ALT} src={ReactLogo} />
+    <h1 className={HomeStyles.heading}>{WELCOME_HEADING}</h1>
+    <div className={HomeStyles.badges}>
       <img alt="License Badges" src={LICENCE_BADGE} />
       <img alt="Stars Badges" src={FORK_BADGE} />
       <img alt="Forks Badges" src={STARS_BADGE} />
     </div>
-  </>
+  </div>
 );
 export default Home;
