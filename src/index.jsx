5ccf34f5209091b7b1ac401584160aa740e0dce6
Anand Gupta <anand.gupta@olx.com>
2022-09-08 15:32:19 +0530

    API to support the concurrent rendering and upgraded storybook to ^6.5.10

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable no-console */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import Routes from './routes/Routes';
 import Store from './redux/store';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={Store}>
     <Routes />
   </Provider>,
-  document.getElementById('root'),
 );
 
 if (!__DEVELOPMENT__ && !__SERVER__) {
